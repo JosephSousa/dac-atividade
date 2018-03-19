6aa152e7725a96a6a1ca82d76ab48b4b2b86023d
JosephSousa <jsantos.te@gmail.com>
2018-03-19 07:42:27 -0300

    adicionando shared

---
+++ insert.sql
@@ -0,0 +1,9 @@
+﻿insert into cliente (email,nome) values('jsantos.te@gmail.com','Joseph');
+insert into cliente (email,nome) values('Matheus@gmail.com','Matheus');
+insert into cliente (email,nome) values('joaquim@gmail.com','Joaquim');
+insert into cliente (email,nome) values('Genesio@gmail.com','Genesio');
+
+insert into produto (descricao,valor) values('tenis',99.99); 
+insert into produto (descricao,valor) values('Camisa',150.00);
+insert into produto (descricao,valor) values('calça',200.00);
+insert into produto (descricao,valor) values('agasalho',300.55);
--- shared/src/main/java/com/dac/shared/Produto.java
+++ shared/src/main/java/com/dac/shared/Produto.java
@@ -1,4 +1,4 @@
-package edu.ifpb.dac;
+package com.dac.shared;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
