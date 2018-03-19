bf3476a9550b077b1719bddf43b7e87b152004fe
matheusluna <matheusluna96@gmail.com>
2018-03-19 10:48:37 -0300

    adicionando jsf

--- order/src/main/java/edu/ifpb/dac/controller/ControladorPedido.java
+++ order/src/main/java/edu/ifpb/dac/controller/ControladorPedido.java
@@ -1,11 +1,29 @@
 package edu.ifpb.dac.controller;
 
+import edu.ifpb.dac.Pedido;
+import edu.ifpb.dac.Produto;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
 /**
  * @brief Classe ControladorPedido
  * @author Joseph Sousa
  * @mail jsantos.te@gmail.com
  * @date   19/03/2018
  */
-public class ControladorPedido {
-
+@Named
+@SessionScoped
+public class ControladorPedido implements Serializable{
+    
+    public List<Produto> produtos;
+    public Pedido pedido = new Pedido();
+    
+    @PostConstruct
+    public void metodoPostConstruct(){
+        
+    }
+    
 }
